afc7246e7f955a80853ee91eb975cdbede46989f
Bryan Xing <bxing@my.bcit.ca>
2020-04-05 15:37:52 -0700

    added join functionality, added new event inputs

--- scripts/create_event.js
+++ scripts/create_event.js
@@ -4,19 +4,26 @@ function writeEventInfo() {
         e.preventDefault();
         let eventName = document.getElementById("inputEventName").value;
         let eventDescrip = document.getElementById("inputEventDescription").value;
+        let eventLocation = document.getElementById("inputEventLocation").value;
         let eventDate = document.getElementById("inputEventDate").value;
         let eventTime = document.getElementById("inputEventTime").value;
         let eventTag = document.getElementById("tags").value;
+        let user = firebase.auth().currentUser.displayName;
+        let memberList = [user];
         
         console.log(eventName);
         console.log(eventDescrip);
+        console.log(eventLocation);
         console.log(eventDate);
         console.log(eventTime);
         console.log(eventTag);
+        console.log(user);
     
         db.collection("events").add({
             Name: eventName,
             Description: eventDescrip,
+            Location: eventLocation,
+            Members: memberList,
             Date: eventDate,
             Time: eventTime,
             Tag: eventTag,
--- scripts/event.js
+++ scripts/event.js
@@ -1,24 +1,56 @@
-console.log(localStorage.getItem("event"));
-
 let docReference = localStorage.getItem("event");
-console.log(db.collection("events")
+let joinButton = document.getElementById("joinButton");
+let memberList = document.getElementById("event-memberList");
+let memberArr;
+
+function getEvent() {
+
+  console.log(localStorage.getItem("event"));
+
+  console.log(db.collection("events")
     .doc(docReference.toString())
     .get()
-    .then(function(doc) {
-        if (doc.exists) {
-            document.getElementById("event-name").innerHTML = doc.data().Name;
-            document.getElementById("event-time").innerHTML = doc.data().Date + "@" + doc.data().Time;
-            document.getElementById("event-location").innerHTML = doc.data().Name; //Get a location and update this
-            document.getElementById("event-description").innerHTML = doc.data().Description;
-
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
+    .then(function (doc) {
+      if (doc.exists) {
+        document.getElementById("event-name").innerHTML = doc.data().Name;
+        document.getElementById("event-time").innerHTML = doc.data().Date + "@" + doc.data().Time;
+        document.getElementById("event-location").innerHTML = doc.data().Location;
+        document.getElementById("event-description").innerHTML = doc.data().Description;
+        memberArr = doc.data().Members;
+        for (let i = 0; i < memberArr.length; i++) {
+          let member = document.createElement("li");
+          member.textContent = memberArr[i];
+          memberList.appendChild(member);
         }
-      }).catch(function(error) {
-        console.log("Error getting document:", error);
+
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }).catch(function (error) {
+      console.log("Error getting document:", error);
 
 
-}))
-    
+    }))
+}
 
+function joinEvent() {
+  let user = firebase.auth().currentUser.displayName;
+  let document = db.collection("events").doc(docReference.toString());
+
+  if (!memberArr.includes(user)) {
+    document.update({
+      Members: firebase.firestore.FieldValue.arrayUnion(user)
+    });
+    while (memberList.firstChild) {
+      memberList.removeChild(memberList.firstChild);
+    }
+    getEvent();
+  } else {
+    console.log("Already joined the event!");
+  }
+}
+
+getEvent();
+
+joinButton.onclick = joinEvent;
